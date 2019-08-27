de153a44e9c0ee5599c6ba3e247689833c6c051e
Yuval Allweil <themarquisdesheric@gmail.com>
2019-08-26 17:21:49 -0700

    remove products page and images

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -5,7 +5,6 @@ import cloudinary from 'netlify-cms-media-library-cloudinary';
 
 import AboutPagePreview from './preview-templates/AboutPagePreview';
 import BlogPostPreview from './preview-templates/BlogPostPreview';
-import ProductPagePreview from './preview-templates/ProductPagePreview';
 import IndexPagePreview from './preview-templates/IndexPagePreview';
 
 CMS.registerMediaLibrary(uploadcare);
@@ -13,5 +12,4 @@ CMS.registerMediaLibrary(cloudinary);
 
 CMS.registerPreviewTemplate('index', IndexPagePreview);
 CMS.registerPreviewTemplate('about', AboutPagePreview);
-CMS.registerPreviewTemplate('products', ProductPagePreview);
 CMS.registerPreviewTemplate('blog', BlogPostPreview);
