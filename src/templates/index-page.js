38f263ab1410f98fbbf3d9a1cdb88e8bd4d6c8b4
Yuval Allweil <themarquisdesheric@gmail.com>
2019-10-01 20:28:30 -0700

    begin styling landing page

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -16,16 +16,28 @@ export const IndexPageTemplate = ({
         })`
       }}
     />
-    <main className="index content flex flex-col items-center">
-      <section className="max-w-xl p-8 text-lg leading-loose md:text-xl">
-        <p>
-          Amberhealth is a comprehensive guide of curated resources
-          for those with chronic illness
-          who are navigating a broken healthcare system
-          while trying to preserve their dignity and self worth
-          and ultimately attain wellness and health.
-        </p>
-      </section>
+    <main className="flex flex-col items-center">
+      <div className="index-wrapper text-justify mb-12">
+        <section className="max-w-xl m-auto px-6">
+          <p>
+            The Chronic is a comprehensive guide
+            of curated resources for those with chronic illness
+            who are navigating a broken healthcare system
+            while trying to preserve their dignity and self worth
+            and ultimately attain wellness and health.
+          </p>
+        </section>
+        <section className="w-screen bg-gray-900 text-white p-6">
+          <p className="max-w-xl">
+            Valuable resources like time, access, and energy are often limited when experiencing illness. This thoughtful collection of trusted resources addresses a variety of diseases and conditions and aims to empower, challenge, and revolutionize the patient perspective.
+          </p>
+        </section>
+        <section className="w-screen p-6 text-white" style={{ background: '#b3564b' }}>
+          <p className="max-w-xl">
+            When seeking medical care it is imperative to receive validation in the form of an accurate diagnosis which leads to an appropriate treatment approach and better quality of care. Being informed to the best of your ability will help you advocate for yourself and your health needs, especially if your doctor is not prepared to do so.
+          </p>
+        </section>
+      </div>
     </main>
   </div>
 );
