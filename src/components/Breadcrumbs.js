802997c9533a9d413849c21444bf23579a829323
Yuval Allweil <themarquisdesheric@gmail.com>
2019-10-06 18:37:21 -0700

    increase nav/breadcrumb margins from tablet up

--- src/components/Breadcrumbs.js
+++ src/components/Breadcrumbs.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
 
 const Breadcrumbs = ({ path }) => (
-  <p className="breadcrumbs" style={{ marginBottom: '3rem' }}>
+  <p className="breadcrumbs">
     <Link to="/">
       Home
     </Link>
