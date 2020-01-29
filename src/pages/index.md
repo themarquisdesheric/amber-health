88dc5579cc1c3d7680085bf452ad3a71dc53cba1
Yuval Allweil <themarquisdesheric@gmail.com>
2020-01-29 07:37:09 -0800

    refactor landing page, still needs snippet love

--- src/pages/index.md
+++ src/pages/index.md
@@ -4,22 +4,15 @@ image: /img/pomwallpaper.png
 heroText: >-
   The Chronic is a comprehensive guide of curated resources for those with
   chronic illness who are navigating a broken healthcare system while trying to
-  preserve their dignity and self worth and ultimately attain wellness and
-  health.
-secondBlockText: >-
-  Valuable resources like time, access, and energy are often limited when
-  experiencing illness. This thoughtful collection of trusted resources
-  addresses a variety of diseases and conditions and aims to empower, challenge,
-  and revolutionize the patient perspective.
-thirdBlockText: >-
-  When seeking medical care it is imperative to receive validation in the form
-  of an accurate diagnosis which leads to an appropriate treatment approach and
-  better quality of care. Being informed to the best of your ability will help
-  you advocate for yourself and your health needs, especially if your doctor is
-  not prepared to do so.
-firstSnippet: curated resources for those with chronic illness
-secondSnippet: your guide for navigating a broken healthcare system
-thirdSnippet: preserve your dignity and self worth
-fourthSnippet: attain wellness and health
+  preserve their dignity and self worth, and ultimately attain wellness and
+  health
+firstSnippetHeader: Healing and Wellness
+firstSnippetText: When seeking medical care it is imperative to receive validation in the form of an accurate diagnosis which leads to the appropriate treatment approach and a better quality of care
+secondSnippetHeader: Medical Literacy
+secondSnippetText: Being informed to the best of your ability will help you advocate for yourself and your health needs, especially if your doctor is not prepared to do so
+thirdSnippetHeader: Patient Advocacy
+thirdSnippetText: This thoughtful collection of trusted resources addresses a variety of diseases and conditions and aims to empower, challenge, and revolutionize the patient perspective
+postSnippetsText: >-
+  After spending a lifetime experiencing illness, adapting to life as a complex patient, and dedicating myself to researching various conditions, I felt compelled to share this cultivation of resources, skills, and insights to aid others on their journey to a better understanding of their conditions, restoring agency, and healing 
 ---
 
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -7,82 +7,86 @@ import Layout from '../components/Layout';
 export const IndexPageTemplate = ({
   image,
   heroText,
-  secondBlockText,
-  thirdBlockText,
-  firstSnippet,
-  secondSnippet,
-  thirdSnippet,
-  fourthSnippet
-}) => (
-  <div>
-    <div className="index-wrapper text-justify mb-12 leading-loose md:text-lg lg:text-xl">
-      <section 
-        className="full-width-image m-auto p-12"
-        style={{
-          backgroundImage: `url(${
-            image.childImageSharp ? image.childImageSharp.fluid.src : image
-          })`,
-          height: 'calc(100vh - 65px)'
-        }}
-      >
-        <div className="pink-modal-wrapper max-w-xl py-4">
-          <p className="pink-modal-text text-md">
-            {heroText}
+  firstSnippetHeader,
+  firstSnippetText,
+  secondSnippetHeader,
+  secondSnippetText,
+  thirdSnippetHeader,
+  thirdSnippetText,
+  postSnippetsText
+}) => {
+  const snippets = [{
+    image: '/img/SliceWhite2.png',
+    header: firstSnippetHeader,
+    text: firstSnippetText
+  },
+  {
+    image: '/img/PomSketchWhite1.png',
+    header: secondSnippetHeader,
+    text: secondSnippetText
+  },
+  {
+    image: '/img/PomSketchWhite2.png',
+    header: thirdSnippetHeader,
+    text: thirdSnippetText
+  }];
+
+  return (
+    <div>
+      <div className="index-wrapper text-justify mb-12 leading-loose md:text-lg lg:text-xl">
+        <section 
+          className="full-width-image m-auto p-12"
+          style={{
+            backgroundImage: `url(${
+              image.childImageSharp ? image.childImageSharp.fluid.src : image
+            })`,
+            height: 'calc(100vh - 65px)'
+          }}
+        >
+          <div className="pink-modal-wrapper max-w-xl py-4">
+            <p className="pink-modal-text text-md">
+              {heroText}
+            </p>
+          </div>
+        </section>
+        
+        <section className="snippets" style={{ background: 'rgb(65, 25, 19)' }}>
+          {snippets.map(({ image, header, text }) => (
+            <div key={header}>
+              <img src={image} alt="pomegranate" />
+              <p className="text-xl text-bold">{header}</p>
+              <p>{text}</p>
+            </div>
+          ))}
+        </section>
+        
+        <section className="p-6" style={{ background: 'rgba(179, 86, 75, 0.5)' }}>
+          <p className="max-w-xl" style={{ color: '#fff' }}>
+            {postSnippetsText}
           </p>
-        </div>
-      </section>
-      <section className="p-6" style={{ background: 'rgb(65, 25, 19)' }}>
-        <p className="max-w-xl" style={{ color: '#fff' }}>
-          {secondBlockText}
-        </p>
-      </section>
-      
-      <div className="snippets">
-        <div>
-          <img src="/img/apple-touch-icon.png" alt="pomegranate logo" />
-          <p>{firstSnippet}</p>
-        </div>
-        <div>
-          <img src="/img/apple-touch-icon.png" alt="pomegranate logo" />
-          <p>{secondSnippet}</p>
-        </div>
-        <div>
-          <img src="/img/apple-touch-icon.png" alt="pomegranate logo" />
-          <p>{thirdSnippet}</p>
-        </div>
-        <div>
-          <img src="/img/apple-touch-icon.png" alt="pomegranate logo" />
-          <p>{fourthSnippet}</p>
-        </div>
+        </section>
       </div>
-      
-      <section className="p-6" style={{ background: 'rgba(179, 86, 75, 0.5)' }}>
-        <p className="max-w-xl" style={{ color: '#fff' }}>
-          {thirdBlockText}
-        </p>
-      </section>
     </div>
-  </div>
-);
+  );
+};
 
 IndexPageTemplate.propTypes = {
   image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
   heroText: PropTypes.string.isRequired,
-  secondBlockText: PropTypes.string.isRequired,
-  thirdBlockText: PropTypes.string.isRequired,
-  firstSnippet: PropTypes.string.isRequired,
-  secondSnippet: PropTypes.string.isRequired,
-  thirdSnippet: PropTypes.string.isRequired,
-  fourthSnippet: PropTypes.string.isRequired
+  firstSnippetHeader: PropTypes.string.isRequired,
+  firstSnippetText: PropTypes.string.isRequired,
+  secondSnippetHeader: PropTypes.string.isRequired,
+  secondSnippetText: PropTypes.string.isRequired,
+  thirdSnippetHeader: PropTypes.string.isRequired,
+  thirdSnippetText: PropTypes.string.isRequired,
+  postSnippetsText: PropTypes.string.isRequired
 };
 
-const IndexPage = ({ data: { markdownRemark: { frontmatter }} }) => {
-  return (
-    <Layout>
-      <IndexPageTemplate {...frontmatter} />
-    </Layout>
-  );
-};
+const IndexPage = ({ data: { markdownRemark: { frontmatter }} }) => (
+  <Layout>
+    <IndexPageTemplate {...frontmatter} />
+  </Layout>
+);
 
 IndexPage.propTypes = {
   data: PropTypes.shape({
@@ -99,12 +103,13 @@ export const pageQuery = graphql`
     markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
       frontmatter {
         heroText
-        secondBlockText
-        thirdBlockText
-        firstSnippet
-        secondSnippet
-        thirdSnippet
-        fourthSnippet
+        firstSnippetHeader
+        firstSnippetText
+        secondSnippetHeader
+        secondSnippetText
+        thirdSnippetHeader
+        thirdSnippetText
+        postSnippetsText
         image {
           childImageSharp {
             fluid(maxWidth: 2048, quality: 100) {
