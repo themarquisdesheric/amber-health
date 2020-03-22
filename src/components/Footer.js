6e670e65cfc3ee2c64928a1e5a9b7c62e4eeca8c
Yuval Allweil <themarquisdesheric@gmail.com>
2020-03-22 15:12:27 -0700

    style footer link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 import SocialLinks from './SocialLinks';
 
@@ -7,7 +8,7 @@ const Footer = () => (
     <SocialLinks />
     <small className="flex justify-center py-4">
       <span>
-        © {new Date().getFullYear()} Amber Robinson
+        © {new Date().getFullYear()} Amber Robinson | website by <Link to="https://www.whoisyuval.com" className="footer-link">yuval allweil</Link>
       </span>
     </small>
   </footer>
